a112fbe9ff5cd92ca15b5b80a9201bee5c5d59ec
clayton-schneider <schneiderc2016@gmail.com>
2022-10-20 15:24:36 -0400

    splide first pass integration for slider

---
+++ src/components/Gallery/Slider.tsx
@@ -0,0 +1,35 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css/core";
+interface Props {
+  images: {
+    src: any;
+    alt: string;
+  }[];
+}
+
+export function Slider({ images }: Props) {
+  return (
+    <Splide
+      className="h-[350px] w-[600px] relative"
+      options={{
+        lazy: true,
+        type: "loop",
+        autoplay: true,
+        gap: "40px",
+        classes: {
+          arrow:
+            "fill-light-blue absolute [&>svg]:w-5 [&>svg]:h-5 z-50 bg-white rounded-full p-2 shadow-xl shadow-neutral-300",
+          next: "right-10 top-1/2",
+          prev: "left-10 top-1/2 rotate-180",
+        },
+      }}
+      aria-label="React Splide Example"
+    >
+      {images.map((image, idx) => (
+        <SplideSlide key={idx}>
+          <img src={image.src} alt={image.alt} className="rounded-[50px]" />
+        </SplideSlide>
+      ))}
+    </Splide>
+  );
+}
