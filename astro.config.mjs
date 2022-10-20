01e788e57d62d4c971ebfff69ba4d9ee8a74e2be
clayton-schneider <schneiderc2016@gmail.com>
2022-10-20 16:13:18 -0400

    build fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,20 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    react(),
+  ],
+  vite: {
+    ssr: {
+      noExternal: ["@splidejs/react-splide"],
+    },
+  },
 });
